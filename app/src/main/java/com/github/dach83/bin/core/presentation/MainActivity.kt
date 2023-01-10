55220cb8e99e62d4c948297fcbe4d56a388057eb
dach83 <dach83@mail.ru>
2023-01-10 13:07:19 +0300

    Arrange package structure

--- app/src/main/java/com/github/dach83/bin/core/presentation/MainActivity.kt
+++ app/src/main/java/com/github/dach83/bin/core/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.github.dach83.bin
+package com.github.dach83.bin.core.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.github.dach83.bin.ui.theme.BINTheme
+import com.github.dach83.bin.core.presentation.ui.theme.BINTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
