db7878a55d594e541d1327dc4f9fd081b613806b
dach83 <dach83@mail.ru>
2023-01-11 11:39:34 +0300

    Implementation for ValidateCardNumber

--- app/src/test/java/com/github/dach83/bin/feature/search/TestModels.kt
+++ app/src/test/java/com/github/dach83/bin/feature/search/TestModels.kt
@@ -3,9 +3,12 @@ package com.github.dach83.bin.feature.search
 import com.github.dach83.bin.feature.search.domain.model.CardDetails
 
 const val EMPTY_CARD_NUMBER = ""
+const val BLANK_CARD_NUMBER = "   "
 const val VISA_CARD_NUMBER = "45717360"
 const val MASTER_CARD_NUMBER = "554386014950"
 const val INVALID_CARD_NUMBER = "INVALID"
+const val MIXED_CARD_NUMBER = "23,xc]["
+const val LONG_CARD_NUMBER = "55438601495012348"
 const val SEARCH_ERROR_MESSAGE = 777
 
 val visaCardDetails = CardDetails(
---
+++ app/src/test/java/com/github/dach83/bin/feature/search/domain/usecase/ValidateCardNumberImplTest.kt
@@ -0,0 +1,62 @@
+package com.github.dach83.bin.feature.search.domain.usecase
+
+import com.github.dach83.bin.feature.search.*
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class ValidateCardNumberImplTest {
+
+    @Test
+    fun `digits card number is valid`() {
+        val sut = ValidateCardNumberImpl()
+
+        val isValid = sut(VISA_CARD_NUMBER)
+
+        assertThat(isValid).isTrue()
+    }
+
+    @Test
+    fun `empty card number is invalid`() {
+        val sut = ValidateCardNumberImpl()
+
+        val isValid = sut(EMPTY_CARD_NUMBER)
+
+        assertThat(isValid).isFalse()
+    }
+
+    @Test
+    fun `blank card number is invalid`() {
+        val sut = ValidateCardNumberImpl()
+
+        val isValid = sut(BLANK_CARD_NUMBER)
+
+        assertThat(isValid).isFalse()
+    }
+
+    @Test
+    fun `letters card number is invalid`() {
+        val sut = ValidateCardNumberImpl()
+
+        val isValid = sut(INVALID_CARD_NUMBER)
+
+        assertThat(isValid).isFalse()
+    }
+
+    @Test
+    fun `mixed card number is invalid`() {
+        val sut = ValidateCardNumberImpl()
+
+        val isValid = sut(MIXED_CARD_NUMBER)
+
+        assertThat(isValid).isFalse()
+    }
+
+    @Test
+    fun `too long card number is invalid`() {
+        val sut = ValidateCardNumberImpl()
+
+        val isValid = sut(LONG_CARD_NUMBER)
+
+        assertThat(isValid).isFalse()
+    }
+}
