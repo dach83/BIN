43dd911be0a5922d8ccb0c5b37a208d3b2764250
dach83 <dach83@mail.ru>
2023-01-11 11:49:57 +0300

    Implementation for RequestCardDetailsImpl

---
+++ app/src/main/java/com/github/dach83/bin/feature/search/domain/usecase/RequestCardDetailsImpl.kt
@@ -0,0 +1,11 @@
+package com.github.dach83.bin.feature.search.domain.usecase
+
+import com.github.dach83.bin.feature.search.domain.model.CardDetails
+import com.github.dach83.bin.feature.search.domain.repository.SearchRepository
+
+class RequestCardDetailsImpl(
+    private val searchRepository: SearchRepository
+) : RequestCardDetails {
+    override suspend fun invoke(cardNumber: String): CardDetails =
+        searchRepository.requestCardDetails(cardNumber)
+}
